5877dd8df797a2ec3d301b178089176645faaa31
Afif Zafri <afzafri@gmail.com>
2017-02-20 16:29:39 +0800

    Created separated demo page, api file only return json

---
+++ demo.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>Fetch Instagram's Photo Data</title>
+<style>
+table {
+    border-collapse: collapse;
+}
+
+table, th, td {
+    border: 1px solid black;
+	padding: 10px;
+	 text-align: left;
+}
+</style>
+</head>
+<body>
+<center>
+<h1>Fetch Instagrams's Photo Data</h1>
+<form action="demo.php" method="get">
+Instagram Photo URL : <input type="text" name="url"><br><br>
+<input type="submit" name="submit">
+</form>
