8461c29934ca8fe39ce7ce4b2a59ede99f72adb1
Afif Zafri <afzafri@gmail.com>
2017-02-20 16:30:10 +0800

    change isset variable name

--- index.php
+++ index.php
@@ -1,29 +1,6 @@
-<html>
-<head>
-<title>Fetch Instagram's Photo Data</title>
-<style>
-table {
-    border-collapse: collapse;
-}
-
-table, th, td {
-    border: 1px solid black;
-	padding: 10px;
-	 text-align: left;
-}
-</style>
-</head>
-<body>
-<center>
-<h1>Fetch Instagrams's Photo Data</h1>
-<form action="index.php" method="get">
-Instagram Photo URL : <input type="text" name="url"><br><br>
-<input type="submit" name="submit">
-</form>
-
 <?php
 
-if(isset($_GET['submit']))
+if(isset($_GET['url']))
 { 
 	$url = $_GET['url'];
 	
